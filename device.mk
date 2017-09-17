17c2b784fb4b40d67094b5591528883a47dfa48d
Ethan Chen <intervigil@gmail.com>
2017-09-16 22:14:36 -0700

    whyred: Add basic USB HAL that reports no status change

--- device.mk
+++ device.mk
@@ -439,7 +439,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-	android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.tulip
 
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk
