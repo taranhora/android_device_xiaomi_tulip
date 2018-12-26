efb7b2b79879639fc2f41736cf01e170eeafd32c
taran107 <hora.cooltaran@gmail.com>
2018-12-26 08:10:21 +0000

    lean bringup

--- lean_tulip.mk
+++ lean_tulip.mk
@@ -19,8 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Lean stuff
+$(call inherit-product, vendor/lean/config/common.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from tulip device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -28,7 +29,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := xiaomi
 PRODUCT_DEVICE := tulip
 PRODUCT_MANUFACTURER := xiaomi
-PRODUCT_NAME := lineage_tulip
+PRODUCT_NAME := lean_tulip
 PRODUCT_MODEL := Redmi Note 6 Pro
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
