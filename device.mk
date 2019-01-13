a8847192fc178e606aa7c5205d456d1776929bec
Advertpavan <pvnptl98@gmail.com>
2019-01-13 16:20:08 +0000

    tulip: updated overlay

--- device.mk
+++ device.mk
@@ -18,14 +18,9 @@
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
-
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
