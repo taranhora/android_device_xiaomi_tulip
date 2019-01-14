f1971fe225e255bdbb778b2600456e33b34c1ad6
Advertpavan <pvnptl98@gmail.com>
2019-01-14 09:08:17 +0000

    build Snap cam

--- device.mk
+++ device.mk
@@ -119,7 +119,8 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    libxml2
+    libxml2 \
+    Snap
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
